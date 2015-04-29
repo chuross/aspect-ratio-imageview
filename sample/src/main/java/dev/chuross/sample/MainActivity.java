d76d269fcc48f4c18e4bdf8a93b7283a9b4748fd
chuross <azarasi.mo.mo@gmail.com>
2015-04-29 16:24:02 +0900

    add sample code

--- sample/src/main/java/dev/chuross/sample/MainActivity.java
+++ sample/src/main/java/dev/chuross/sample/MainActivity.java
@@ -2,6 +2,9 @@ package dev.chuross.sample;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setImageViewSize(R.id.img_aspect_image_1, R.id.txt_size_1);
+        setImageViewSize(R.id.img_aspect_image_2, R.id.txt_size_2);
+        setImageViewSize(R.id.img_aspect_image_3, R.id.txt_size_3);
+        setImageViewSize(R.id.img_aspect_image_4, R.id.txt_size_4);
+        setImageViewSize(R.id.img_aspect_image_5, R.id.txt_size_5);
+        setImageViewSize(R.id.img_aspect_image_6, R.id.txt_size_6);
+        setImageViewSize(R.id.img_aspect_image_7, R.id.txt_size_7);
+        setImageViewSize(R.id.img_aspect_image_8, R.id.txt_size_8);
+        setImageViewSize(R.id.img_aspect_image_9, R.id.txt_size_9);
+        setImageViewSize(R.id.img_aspect_image_10, R.id.txt_size_10);
+    }
+
+    private void setImageViewSize(int imageViewId, int sizeTextViewId) {
+        final TextView text = (TextView) findViewById(sizeTextViewId);
+        final View imageView = findViewById(imageViewId);
+        imageView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                imageView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                text.setText(String.format("w:%d x h:%d", imageView.getWidth(), imageView.getHeight()));
+            }
+        });
     }
 }
